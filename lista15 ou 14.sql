9804788f51266cb15655864b6f52c390c100be32
Danilo <danilocarcosta@hotmail.com>
2023-10-26 16:48:55 -0300

    Projeto final bd update

--- lista15 ou 14.sql
+++ lista15 ou 14.sql
@@ -1,7 +1,9 @@
 -- Criação do banco de dados:
 CREATE DATABASE IndustriaBeleza;
 -- Abertua do banco
-USE DATABASE IndustriBeleza;
+USE IndustriaBeleza;
+use master
+drop database IndustriaBeleza
 
 -- Criação das tabelas:
 
@@ -37,7 +39,7 @@ CREATE TABLE Cliente(
 -- ModeloVeiculo (PK: codModeloVeiculo)
 CREATE TABLE ModeloVeiculo(
     PRIMARY KEY (codModeloVeiculo),
-    codModeloVeiculo int IDENTITY
+    codModeloVeiculo int IDENTITY,
     descricaoModelo varchar(60)
 );
 
@@ -46,7 +48,8 @@ CREATE TABLE Veiculo(
 	PRIMARY KEY (codVeiculo),
     FOREIGN KEY (codModeloVeiculo) REFERENCES ModeloVeiculo,
 	codVeiculo int IDENTITY,
-	descricaoModelo int,
+	codModeloVeiculo int,
+	descricaoModelo varchar(60),
 	placaVeiculo varchar(8)
 );
 
@@ -62,12 +65,13 @@ CREATE TABLE Telefone(
     FOREIGN KEY (codCliente) REFERENCES Cliente,
     FOREIGN KEY (CodTipoFone) REFERENCES TipoFone,
     CodTipoFone int,
+	CodCliente int,
     numeroTelefone varchar(15)
 );
 
 -- PontoEstrategico (FK: codRegiao)
 CREATE TABLE PontoEstrategico(
-    FOREIGN KEY (codRegiao),
+    FOREIGN KEY (codRegiao) REFERENCES Regiao,
     codRegiao int,
     nomePontoEst varchar(60),
     descricaoPontoEst varchar(60)
@@ -80,7 +84,8 @@ CREATE TABLE Vendedor(
     FOREIGN KEY (codRegiao) REFERENCES Regiao,
     codVeiculo int,
     codRegiao int,
-    codVendedor int IDENTITY    
+    codVendedor int IDENTITY,
+	nomeVendedor varchar(60)
 );
 
 -- AtribuicaoVeiculoVendedor
@@ -90,7 +95,7 @@ CREATE TABLE AtribuicaoVeiculoVendedor(
     FOREIGN KEY (codVendedor) REFERENCES Vendedor,
     codAtribuicao int IDENTITY,
     codVeiculo int,
-    codCliente int,
+    codVendedor int,
     dataAtribuicao date
 );
 
@@ -100,12 +105,16 @@ CREATE TABLE NotaFiscal(
     FOREIGN KEY (codVendedor) REFERENCES Vendedor,
     FOREIGN KEY (codCliente) REFERENCES Cliente,
     codNotaFiscal int IDENTITY,
+	codVendedor int,
+	codCliente int,
 );
 
 -- DescricaoNota (FK: codProduto & codNotaFiscal)
 CREATE TABLE DescricaoNota(
     FOREIGN KEY (codNotaFiscal) REFERENCES NotaFiscal,
     FOREIGN KEY (codProduto) REFERENCES Produto,
+	codNotaFiscal int,
+	codProduto int,
     descricaoNota varchar(150)
 );
 
@@ -120,75 +129,122 @@ VALUES
 ('Centro', 'Centro', 'Rio de Janeiro'),
 ('Santo Amaro', 'Subúrbio', 'Salvador'),
 ('Copacabana', 'Litoral', 'Rio de Janeiro');
+select * from Regiao;
 
 -- Produto
 INSERT INTO Produto (nomeProduto, valorProduto, estoqueProduto)
 VALUES
-('Batom vermelho', 10, 1000),
-('');
+('Batom', 10, 1000),
+('Esmalte', 5, 500),
+('Shampoo', 8, 800),
+('Condicionador', 7, 600),
+('Perfume', 20, 300),
+('Lápis de olho', 4, 1200);
+select * from Produto;
 
 -- Cliente
 INSERT INTO Cliente (nomeCliente, foneCliente, dataAniversario, cpfCliente)
 VALUES
-('Danilo', '(11) 91326-2429', '01/08/2005', '439.797.828-08'),
-('');
+('Danilo', '(11) 91326-2429', '01/08/2005', '549.828.235-08'),
+('Maria', '(21) 98765-4321', '1990-03-15', '123.456.789-00'),
+('João', '(71) 55555-1212', '1985-11-20', '987.654.321-01'),
+('Ana', '(11) 5555-5555', '1995-07-10', '111.222.333-44'),
+('Carlos', '(21) 1234-5678', '1980-04-05', '555.666.777-88'),
+('Paula', '(71) 9999-8888', '2002-09-30	', '777.888.999-00');
+select * from Cliente;
 
 -- Modelo do Veículo
 INSERT INTO ModeloVeiculo (descricaoModelo)
 VALUES
-('Palio Weekend'),
-('');
+('Palio'),
+('Gol'),
+('Celta'),
+('Fiesta'),
+('Civic'),
+('Corolla');
+select * from ModeloVeiculo;
 
 -- Veículo
-INSERT INTO Veiculo (codModeloVeiculo, placaVeiculo)
+INSERT INTO Veiculo (codModeloVeiculo, descricaoModelo, placaVeiculo)
 VALUES
-(1, 'ADM-1234'),
-('');
+(1, 'Vermelho', 'ADM-1234'),
+(2, 'Branco', 'XYZ-5678'),
+(3, 'Preto', 'ABC-9876'),
+(4, 'Azul', 'QWE-4567'),
+(5, 'Prata', 'JKL-7890'),
+(6, 'Prata', 'MNO-1234');
+select * from Veiculo;
 
 -- Tipo do telefone
 INSERT INTO TipoFone (descricaoTipoFone)
 VALUES
 ('Celular'),
-('Telefone fixo');
+('Telefone fixo'),
+('Comercial');
+select * from TipoFone;
 
 -- Telefone
-INSERT INTO Telefone (CodTipoFone, numeroTelefone)
+INSERT INTO Telefone (CodCliente, CodTipoFone, numeroTelefone)
 VALUES
-(1, '(11) 1234-21345'),
-('');
-
--- Produto
-INSERT INTO Produto (nomeProduto, valorProduto, estoqueProduto)
-VALUES
-('Batom vermelho', 10, 1000),
-('');
+(1, 1, '(11) 1234-21345'),
+(2, 1, '(21) 9876-5432'),
+(3, 1, '(71) 5555-1212'),
+(4, 1, '(11) 4444-4444'),
+(5, 1, '(11) 2054-2880'),
+(6, 1, '(11) 2059-6598');
+select * from Telefone;
 
 -- Ponto estratégico
 INSERT INTO PontoEstrategico (codRegiao, nomePontoEst, descricaoPontoEst)
 VALUES
-(1, 'Loja Casas Bahia', 'Travessa da Avenida Marechal Tito'),
-('');
+(1, 'Lojas Casas Bahia', 'Avenida Marechal Tito'),
+(2, 'Supermercado ABC', 'Avenida Central'),
+(3, 'Farmácia Pague Menos', 'Rua Principal'),
+(4, 'Loja de Conveniência', 'Praça da República'),
+(5, 'Posto de Gasolina', 'Avenida da Independência'),
+(6, 'Shopping Center', 'Centro Comercial');
+select * from PontoEstrategico;
 
 -- Vendedor
-INSERT INTO Vendedor (codVeiculo)
+INSERT INTO Vendedor (codVeiculo, codRegiao, nomeVendedor)
 VALUES
-(1, 'Loja Casas Bahia', 'Travessa da Avenida Marechal Tito'),
-('');
+(1, 1, 'Daniel'),
+(2, 2, 'Guilherme'),
+(3, 3, 'Gabriel'),
+(4, 4, 'Gustavo'),
+(5, 5, 'Nicolas'),
+(6, 6, 'Lucas');
+select * from Vendedor;
 
 -- Atribuição de um veículo ao vendedor
 INSERT INTO AtribuicaoVeiculoVendedor (codVeiculo, codVendedor, dataAtribuicao)
 VALUES
-(1, 1, '01/02/2010'),
-('');
+(1, 1, '2010-02-01'),
+(2, 2, '2011-05-15'),
+(3, 3, '2012-08-20'),
+(4, 4, '2013-11-25'),
+(5, 5, '2014-06-10'),
+(6, 6, '2015-09-15');
+select * from AtribuicaoVeiculoVendedor;
 
 -- Nota Fiscal
 INSERT INTO NotaFiscal (codVendedor, codCliente)
 VALUES
 (1, 1),
-('');
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6);
+select * from NotaFiscal;
 
 -- Descrição da nota fiscal
-INSERT INTO descricaoNota (codNotaFiscal, codProduto, descricaoNota)
+INSERT INTO DescricaoNota (codNotaFiscal, codProduto, descricaoNota)
 VALUES
 (1, 1, 'Débito'),
-('');
+(2, 3, 'Crédito'),
+(3, 2, 'Débito'),
+(4, 4, 'Crédito'),
+(5, 5, 'Débito'),
+(6, 6, 'Crédito');
+select * from DescricaoNota;
